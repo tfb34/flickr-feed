fc3cece2cb5f622f88c7beb453a8e8a04d961165
tfb34 <britothalia212@gmail.com>
2017-08-01 14:13:55 -0400

    Build a simple StaticPagesController to display a home page with a simple form

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+	def index
+		@flickr_id = params[:flickr_id]
+	end
+
+	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#index'
+  root 'static_pages#index'
+  post '/photos', to: 'static_pages#index'
+  get '/photos', to: 'static_pages#index'
 end
