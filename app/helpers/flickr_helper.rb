43f6c83a2b14d8535ebb9552afddb61aea1c1440
tfb34 <britothalia212@gmail.com>
2017-08-02 19:49:12 -0400

    Add design and page layout

--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -5,12 +5,12 @@ module FlickrHelper
     FlickRaw.api_key = ENV["API_KEY"]
     FlickRaw.shared_secret = ENV["SHARED_SECRET"]
 
-    flickr.photos.search(:user_id => user_id).to_a.values_at(0..photo_count)
+    flickr.photos.search(:user_id => user_id).to_a.values_at(0..photo_count-1)
   end
   
   def render_flickr_sidebar_widget(user_id, photo_count = 12, columns = 2)
     begin
-      photos = user_photos(user_id, photo_count).in_groups_of(2)
+      photos = user_photos(user_id, photo_count).in_groups_of(4)
       render :partial => '/flickr/sidebar_widget', :locals => { :photos => photos }
     rescue Exception
       render :partial => '/flickr/unavailable'
